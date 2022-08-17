9b861068bfb63c1e5a485f4e3562e869c9928081
marcossilva <marcos.dev1998@gmail.com>
2022-08-17 12:08:25 -0300

    create(comment): template

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,7 +3,7 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
-import Rails from "@rails/ujs"
+import Rails, { $ } from "@rails/ujs"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 import "bootstrap/dist/js/bootstrap.bundle"
@@ -12,3 +12,8 @@ import "../src/application.css";
 
 Rails.start()
 ActiveStorage.start()
+
+var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+  return new bootstrap.Tooltip(tooltipTriggerEl)
+})
